7a817ec42d846effd077820b40a0af73103a9957
dannyedison75 <proyectosdanny75@gmail.com>
2024-05-12 17:45:21 -0500

    CodigoOriginal

---
+++ js/footer.js
@@ -0,0 +1,56 @@
+const footer = document.getElementById('footer');
+
+footer.innerHTML = ` 
+    <div class="row">
+                <div class="col-lg-4">
+                    <div class="elemeto-titulo1">
+                        <h3>Sedes</h3>
+                        <div class = "raya">
+                            <hr/>
+                        </div>
+                    </div>
+                    
+                    <div class="elemento-informacion1">
+                        <h5>>Medellín: Calle 49 41 - 9.</h5>
+                        <h5>>Bello: Diagonal 50a #38-20 (Tierragro), piso 5.</h5>
+                        <h5>>Rionegro: Carrera 55A # 35-229 Parque de Comfama.</h5>
+                    </div>
+
+                </div>
+                <div class="col-lg-4">
+                    <div class="elemeto-titulo1">
+                        <h3>Líneas de atención</h3>
+                        <div class = "raya">
+                            <hr/>
+                        </div>
+                    </div>
+                    
+                    <div class="elemento-informacion1">
+                    
+                        <h5>>Medellín Área Comercial: (60)(4) 2291100</h5>
+                        <h5>>Medellín Área Administrativa: (60)(4) 4808822</h5>
+                        <h5>>Bogotá: (60)(1) 4823318 - 322 395 4352</h5>
+                        <h5>><a href="#"> comunicaciones@cesde.edu.co </a></h5>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="elemeto-titulo1">
+                        <h3>Redes sociales</h3>
+                        <div class = "raya">
+                            <hr/>
+                        </div>
+                    </div>
+                    
+                    <div class="elemento-informacion1">
+                        
+                        <h5>><a href="#"> comunicaciones@cesde.edu.co </a></h5>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="reservado">
+                <p>(c) 2024 Proyecto Integrador. Todos los derechos reservados.</p>
+                <p>Desarrollo de Software</p>
+            </div>
+`;
---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+
+
+
+/*
+header.innerHTML = ` 
+    <div class="logo">
+        <a href="/index.html">Logo</a>
+        <img src="hola.png" alt=" pruebas" width="150" height="150" align="left">
+        <h1>Mi sitio web</h1>
+    </div>
+
+`;
+*/
+
+/*
+header.innerHTML = ` 
+    <div class="logo">
+        <a href="/index.html">Logo</a>
+    </div>
+
+    <img src="Logo 3D SAS.png" alt="Logotipo de la empresa">
+    <h1>Mi sitio web</h1>
+
+    <div id="nav">
+    <a href="index.html"> Inicio</a>
+    <a href="#contactenos"> Contactenos</a>
+    <a href="#nosotros"> Nosotros</a>
+</div>
+
+`;*/
+
+
+/*
+nav.innerHTML = `
+    
+`;
+
+
+*/
+
